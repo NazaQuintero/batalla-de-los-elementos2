a9bf7a49ed02391661d11ce307078a47faee3173
kamalhosain <khosain@fi.uba.ar>
2021-02-15 18:16:23 -0300

    main rollback

--- main.cpp
+++ main.cpp
@@ -10,25 +10,39 @@
 #include "terminal/Terminal.h"
 #include "validaciones/Validador.h"
 #include "utilitario/Utilitario.h"
-// pruebas
-#include "tablero/Tablero.h"
 
-// Menu* Menu::menu = 0;
-// Terminal* Terminal::terminal = 0;
-// Validador* Validador::validador = 0;
+Menu* Menu::menu = 0;
+Terminal* Terminal::terminal = 0;
+Validador* Validador::validador = 0;
 Utilitario* Utilitario::utilitario = 0;
-
-
-
 
 const string TEXTO_SOLICITUD_OPCION = "Por favor, ingrese una opcion para continuar:";
 
 int main() {
 
-	Tablero* tablero = new Tablero();
+	srand(time(0));
 
-	tablero->mostrarTablero();
+	Diccionario<string, Personaje*> *diccionarioDePersonajes = new Diccionario<string, Personaje*>();
 
+	Utilitario* utilitario = Utilitario::obtenerInstancia();
+
+	utilitario->cargarPersonajes(diccionarioDePersonajes);
+
+	Menu* menu = Menu::obtenerInstancia();
+
+	menu->mostrarOpciones();
+
+	int opcion = menu->obtenerOpcion(TEXTO_SOLICITUD_OPCION);
+
+	int systemResult = menu->validarOpcion(opcion, diccionarioDePersonajes, TEXTO_SOLICITUD_OPCION);
+
+	cout << "El sistema ha finalizado con codigo de error: " << systemResult << "." << endl;
+
+	utilitario->guardarPersonajes(diccionarioDePersonajes);
+
+	delete diccionarioDePersonajes;
+	delete menu;
+	delete utilitario;
 
 	return 0;
 }
